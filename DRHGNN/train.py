b73f13bfcde68d9484ad94d77f52fa5214221090
yunfighting <110522098+yunfighting@users.noreply.github.com>
2023-10-02 00:10:10 +0800

    train.py

--- DRHGNN/train.py
+++ DRHGNN/train.py
@@ -3,6 +3,9 @@ import torch.nn.functional as F
 import path, os
 from utils.utils import *
 from models import *
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
 import config
 # 配置参数
 args = config.config()
@@ -52,12 +55,9 @@ else:
     yvals = torch.index_select(y, 0, torch.LongTensor(mask_val))
 
 
-
 # init H and X
 tmpDataDir = path.Path(f'data/{args.dataname}')
 tmpDataDir.makedirs_p()
-
-
 
 if args.balanced:
     Htrs = [
@@ -95,6 +95,7 @@ nclass = y.max().item() + 1
 
 nlayer, nhid, dropout = args.nlayer, args.nhid, args.dropout
 
+
 Models = {
     'HGNN': HGNN,
     'ResHGNN': ResHGNN,
@@ -110,10 +111,7 @@ schedulartr = torch.optim.lr_scheduler.MultiStepLR(optimizertr, milestones=[100]
 
 #### config the output directory
 dataname = args.dataname
-if args.balanced:
-    out_dir = path.Path( f'./{args.out_dir}/{model_name}_{nlayer}_{dataname}_{split_ratio}/{seed}_balanced' )
-else:
-    out_dir = path.Path(f'./{args.out_dir}/{model_name}_{nlayer}_{dataname}_{split_ratio}/{seed}')
+out_dir = path.Path( f'./{args.out_dir}/{model_name}_{nlayer}_{dataname}_{split_ratio}/{seed}' )
 
 import shutil 
 if out_dir.exists():
@@ -129,14 +127,14 @@ def train():
     modeltr.train()
     optimizertr.zero_grad()
     if args.dataname == 'D4_five_methods'or'D3_five_methods'or'D2_five_methods'or'D1_five_methods':
-        pred = modeltr(Xtrs, Htrs)
+        pred = modeltr(Xtrs, Htrs,device)
 
     loss = F.nll_loss(pred[idx_tr], ytrs[idx_tr])
     loss.backward()
     optimizertr.step()
     if schedulartr: schedulartr.step()
 
-    _train_acc = accuracy(pred[idx_tr], ytrs[idx_tr]) # 2023.3
+    _train_acc = accuracy(pred[idx_tr], ytrs[idx_tr])  # 2023.3
 
     return _train_acc,loss  # 2023.3
 
@@ -145,23 +143,53 @@ def val():
     modeltr.eval()
 
     if args.dataname == 'D4_five_methods'or'D3_five_methods'or'D2_five_methods'or'D1_five_methods': # 2023.3
-        pred = modeltr(Xvals, Hvals) # 2023.3
+        pred = modeltr(Xvals, Hvals,device) # 2023.3
 
     _val_acc = accuracy(pred[idx_val], yvals[idx_val]) # 2023.3
 
-    return  _val_acc # 2023.3
+    # 2023.10.1
+    accuracies = []
+    for class_label in range(nclass):
+        class_acc = class_accuracy(pred[idx_val], yvals[idx_val], class_label)
+        accuracies.append(class_acc)
+    accuracies_tensor = torch.tensor(accuracies)
+
+    # 计算每个类别的 F1-score 和整体的 F1-score 2023.10.1
+    f1_scores_tensor, macro_f1 = calculate_f1_score(pred[idx_val], yvals[idx_val])
+    # 计算每个类别的 MCC 和整体的 MCC 2023.10.1
+    overall_mcc, class_mccs_tensor = calculate_mcc(pred[idx_val], yvals[idx_val])
+    return _val_acc, accuracies_tensor, macro_f1, f1_scores_tensor, overall_mcc, class_mccs_tensor  # 2023.10.1
 
 
 best_acc, badcounter = 0.0, 0
+best_accuracies = torch.tensor([])# 2023.10
+best_macro_f1 = 0.0 # 2023.10.1
+best_f1_scores = torch.tensor([])# 2023.10.1
+best_overall_mcc = 0.0 # 2023.10.1
+best_class_mccs = torch.tensor([])# 2023.10.1
 
 for epoch in range(1, args.epochs+1):
     train_acc, loss = train()  # 2023.3
-    val_acc = val() # 2023.3
+    val_acc, accuracies, macro_f1, f1_scores, overall_mcc, class_mccs = val() # 2023.10.1
     if val_acc > best_acc:
         best_acc = val_acc
-        badcounter = 0 
+        best_accuracies = accuracies  # 2023.10
+        badcounter = 0
     else:
         badcounter += 1
-    if badcounter > args.patience: break
-    baselogger.info(f'Epoch: {epoch}, Loss: {loss:.4f}, Train:{train_acc:.2f}, Val:{val_acc:.2f}, Best Val acc:{best_acc:.3f}' )
+    #  2023.10.1
+    if macro_f1 > best_macro_f1:
+        best_macro_f1 = macro_f1
+        best_f1_scores = f1_scores
+    #  2023.10.1
+    if overall_mcc > best_overall_mcc:
+        best_overall_mcc = overall_mcc
+        best_class_mccs = class_mccs
 
+    if badcounter > args.patience: break
+    # 2023.10.1
+    best_accuracies_str = ', '.join([f'{acc:.5f}' for acc in best_accuracies.tolist()])
+    best_f1_scores_str = ', '.join([f'{f1:.5f}' for f1 in best_f1_scores.tolist()])
+    best_class_mccs_str = ', '.join([f'{mcc:.5f}' for mcc in best_class_mccs.tolist()])
+    baselogger.info(
+        f'Epoch: {epoch}, Loss: {loss:.4f}, Train:{train_acc:.3f}, Val:{val_acc:.3f}, Best Val acc:{best_acc:.5f}, Best accs:[{best_accuracies_str}, Best macro f1:{best_macro_f1:.5f}, Best f1s:[{best_f1_scores_str}, best overall mcc:{best_overall_mcc:.5f}, Best mccs:[{best_class_mccs_str}]')
